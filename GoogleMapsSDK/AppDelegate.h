0fe1a67020ebefa4bf7b96331c193abd582f711b
Diogo Carneiro <contato@idiogo.com.br>
2013-03-04 16:48:27 -0300

    Initial Commit

---
+++ GoogleMapsSDK/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoogleMapsSDK
+//
+//  Created by Diogo Carneiro on 04/03/13.
+//  Copyright (c) 2013 PicPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoogleMapsSDK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GoogleMapsSDK
+//
+//  Created by Diogo Carneiro on 04/03/13.
+//  Copyright (c) 2013 PicPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
